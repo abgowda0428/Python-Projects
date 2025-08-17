e83821a31f1b3de34abbba2157384e4e73a961e8
AB_GOWDA <abhishekgowda8090@gmail.com>
2025-08-17 13:22:06 +0530

    Enhance the User Experience

--- Simpl Cal/main.py
+++ Simpl Cal/main.py
@@ -8,17 +8,17 @@ userExist = True
 
 while userExist :
 
-    userInputOperation = input("Chosse Operation for Calculation or Type 'Exist' to End Programm : (+ - * /) => ")
+    userInputOperation = input("Chosse Operation for Calculation or Type 'exist' to End Programm : (+ - * /) => ")
 
-    if userInputOperation == "Exist":
+    if userInputOperation.lower() == "exist":
         print("Your Existed from the Calculator, have great day :) ")
         userExist = False
     else:
 
         if userInputOperation in Operations:
 
-            userInputNumbers_1 = int(input("Take any first Numbers.."))
-            userInputNumbers_2 = int(input("Take the second Number.."))
+            userInputNumbers_1 = float(input("Take any first Numbers.."))
+            userInputNumbers_2 = float(input("Take the second Number.."))
 
             if userInputOperation == "+":
                 print("The Output For Addition is :",userInputNumbers_1 + userInputNumbers_2)
@@ -34,7 +34,13 @@ while userExist :
         else:
             print("Enter the Operations Given in Prompt..")
 
+        WantToContinue = input("Do You want to Continue, Y/N ? ")
 
+        if WantToContinue.lower() != "y":
+            print("Your Existed from the Calculator, have great day :) ")
+            userExist = False
+        else:
+            print("I am Ready for Next Operation...")
 
 
 
