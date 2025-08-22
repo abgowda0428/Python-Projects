908202bf2a9051f3663aef34304eadccf76c17d2
AB_GOWDA <abhishekgowda8090@gmail.com>
2025-08-22 21:16:27 +0530

    Modification in the StuMang, and Intial Commit in the OOps Project P1

--- StuMangSys/StuManagSys.py
+++ StuMangSys/StuManagSys.py
@@ -8,14 +8,19 @@ Students_Details = {}
 
 def add_Student():
 
-    U_Id = str(uuid.uuid4())[:0]
+    U_Id = str(uuid.uuid4())[:8]
+    PU_Id = tuple(U_Id,)
     user_details = {}
 
     for key in keys:
         value = input(f"Enter Student {key} : ")
         user_details[key] = value
     
-    Students_Details[U_Id] = user_details
+    Students_Details[PU_Id] = user_details
+
+def View_Details():
+    for dictKeys, dictVal in Students_Details.items():
+        print(f"User ID: {dictKeys}, Keys: {list(dictVal.keys())}")
 
 def StartApplications():
 
@@ -27,3 +32,13 @@ def StartApplications():
     print("Enter 4: For Update the Student Details.")
     print("Enter 5: For View all Students Details.")
     print("Enter 'Exit' to Close the Application.")
+    user_input = input("Select from (1 - 5) ? ")
+
+    if user_input == "1":
+        add_Student()
+    elif user_input == "5":
+        View_Details()
+        
+
+StartApplications()
+print(Students_Details.keys())
